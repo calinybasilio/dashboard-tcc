e912822bf88f2b5a83a56d0483b7e0fe97ef089e
caliny <oliveiracaliny@gmail.com>
2022-12-13 08:34:26 -0300

    ajuste environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dashboard-tcc-api-production.up.railway.app/'
+  apiUrl: 'https://dashboard-tcc-api-production.up.railway.app'
 };
